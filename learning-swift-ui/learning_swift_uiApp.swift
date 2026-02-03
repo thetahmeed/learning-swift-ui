68bea5998339535fde0a8b2af529fcfd0b9a33d0
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2026-02-03 12:07:57 +0600

    Initial Commit

---
+++ learning-swift-ui/learning_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swift_uiApp.swift
+//  learning-swift-ui
+//
+//  Created by Tahmeedul Islam on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct learning_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
